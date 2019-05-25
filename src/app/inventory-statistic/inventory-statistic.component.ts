abbc01a52f1095479857c2ab9195b5d3da5dab3d
HaBinh <lethihabinh@gmail.com>
2019-05-25 08:30:38 +0700

    add relative time

--- src/app/inventory-statistic/inventory-statistic.component.ts
+++ src/app/inventory-statistic/inventory-statistic.component.ts
@@ -3,6 +3,8 @@ import { StoreService } from 'app/store/store.service';
 import { Inventory } from 'app/store/inventory.model';
 import {getLangUrl} from "../shared/get_url_lang";
 import {Subject} from "rxjs/Subject";
+import * as moment from 'moment';
+import 'moment/locale/vi';
 
 @Component({
   selector: 'app-inventory-statistic',
@@ -35,6 +37,10 @@ export class InventoryStatisticComponent implements OnInit {
         console.log(this.listInventories);
         this.dtTrigger.next();
       });
+  }
+
+  getRelativeTime(created_at: string) {
+    return moment(created_at).fromNow();
   }
 
 }
